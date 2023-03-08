46fb3eee38cfe071ee8904a0a1fc76f8df418d1a
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-08 11:38:21 +0530

    Mar 8th Notes

--- Hello.java
+++ Hello.java
@@ -1,8 +1,17 @@
+// this is a java program to print Hello world on console
+/* 
+this is developed by Riyaz
+reviewed by Karim
+*/
+ 
 public class Hello
 {
 
 public static void main(String[] args)
 {
+
+//the below statement is for printing
+
 
 System.out.println("Hello World");
 
---
+++ Sample.java
@@ -0,0 +1,20 @@
+//this program is to take some integer and print the value
+import java.util.Scanner;
+public class Sample
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("Enter a value for x");
+
+Scanner sc=new Scanner(System.in);
+
+int x=sc.nextInt();
+
+System.out.println("You have entered a value for x as "+x);
+
+}
+
+
+}
