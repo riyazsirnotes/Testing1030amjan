3c17f12fa16736c6df228d02ea08324d638a62b6
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-10 11:32:04 +0530

    Mar 10th Notes

---
+++ javapractise/VariablesDemo1.java
@@ -0,0 +1,16 @@
+class VariablesDemo1
+{
+
+public static void main(String[] args)
+{
+
+//x is local variable
+int x=10;
+
+System.out.println("value of x is "+x);
+
+
+}
+
+
+}
---
+++ javapractise/VariablesDemo2.java
@@ -0,0 +1,17 @@
+class VariablesDemo2
+{
+
+public static void main(String[] args)
+{
+
+//x is local variable
+//declaration of local varibale
+int x=0;
+
+System.out.println("value of x is "+x);
+
+
+}
+
+
+}
